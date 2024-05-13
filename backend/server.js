ad97e45f4bb5c6ff0d5ac8bccfa112a93c423f83
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-05-13 23:24:14 +0430

    complete first part of user controllers

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
-import authRoutes from "./routes/auth.routes.js";
-import dotenv from "dotenv";
-import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 const app = express();
@@ -14,6 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
