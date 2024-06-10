5ef75f26c60252e927b64dbddbd834d50b45f520
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-06-10 13:48:48 +0430

    UI design complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 
+import RightPanel from "./components/common/RightPanel";
+import Sidebar from "./components/common/Sidebar";
+
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/notification" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
+      </Routes>
+      <RightPanel />
+    </div>
+  );
 }
-
-export default App
---
+++ frontend/src/components/common/Posts.jsx
@@ -0,0 +1,27 @@
+import Post from "./Post";
+import PostSkeleton from "../skeletons/PostSkeleton";
+import { POSTS } from "../../utils/db/dummy";
+
+export default function Posts () {
+	const isLoading = false;
+
+	return (
+		<>
+			{isLoading && (
+				<div className='flex flex-col justify-center'>
+					<PostSkeleton />
+					<PostSkeleton />
+					<PostSkeleton />
+				</div>
+			)}
+			{!isLoading && POSTS?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
+			{!isLoading && POSTS && (
+				<div>
+					{POSTS.map((post) => (
+						<Post key={post._id} post={post} />
+					))}
+				</div>
+			)}
+		</>
+	);
+};
