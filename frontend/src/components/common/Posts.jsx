0f4de6bc176e4e9deceb10d420d76f0cdc4124da
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-06-15 22:27:55 +0430

    fetch posts for the home page

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,27 +1,64 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
-import { POSTS } from "../../utils/db/dummy";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
-export default function Posts () {
-	const isLoading = false;
+export default function Posts({ feedType }) {
+  const getPostEndpoint = () => {
+    switch (feedType) {
+      case "forYou":
+        return "/api/post/all";
+      case "following":
+        return "/api/post/following";
+      default:
+        return "/api/post/all";
+    }
+  };
 
-	return (
-		<>
-			{isLoading && (
-				<div className='flex flex-col justify-center'>
-					<PostSkeleton />
-					<PostSkeleton />
-					<PostSkeleton />
-				</div>
-			)}
-			{!isLoading && POSTS?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
-			{!isLoading && POSTS && (
-				<div>
-					{POSTS.map((post) => (
-						<Post key={post._id} post={post} />
-					))}
-				</div>
-			)}
-		</>
-	);
-};
+  const POST_ENDPOINT = getPostEndpoint();
+
+  const { data:posts, isLoading, refetch, isRefetching } = useQuery({
+    queryKey: ["posts"],
+    queryFn: async () => {
+      try {
+        const res = await fetch(POST_ENDPOINT);
+        const data = await res.json();
+
+        if (!res.ok) {
+          return new Error(data.error || "Something went wrong");
+        }
+
+		return data
+
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+  });
+
+  useEffect(()=>{
+	refetch()
+  },[feedType, refetch])
+
+  return (
+    <>
+      {(isLoading || isRefetching) && (
+        <div className="flex flex-col justify-center">
+          <PostSkeleton />
+          <PostSkeleton />
+          <PostSkeleton />
+        </div>
+      )}
+      {!isLoading && !isRefetching && posts?.length === 0 && (
+        <p className="text-center my-4">No posts in this tab. Switch 👻</p>
+      )}
+      {!isLoading && !isRefetching && posts && (
+        <div>
+          {posts.map((post) => (
+            <Post key={post._id} post={post} />
+          ))}
+        </div>
+      )}
+    </>
+  );
+}
--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -37,7 +37,7 @@ export default function HomePage() {
 				<CreatePost />
 
 				{/* POSTS */}
-				<Posts />
+				<Posts feedType={feedType} />
 			</div>
 		</>
 	);
