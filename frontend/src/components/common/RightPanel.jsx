45c8fe01a0d3dbddf4de4026e1c0ee6290923b0c
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2024-06-17 19:39:05 +0430

    useFollow hook added

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,25 +1,31 @@
 import { Link } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+
+import useFollow from "../../hooks/useFollow";
+
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
-import {useQuery} from '@tanstack/react-query'
+import LoadingSpinner from "./LoadingSpinner";
 
 export default function RightPanel() {
-	const {data: suggestedUsers, isLoading} = useQuery({
-		queryKey: ['suggestedUsers'],
-		queryFn: async ()=>{
-			try {
-				const res= await fetch ('/api/user/suggested')
-				const data= await res.json()
-				if(!res.ok){
-					throw new Error(data.error || "Something went wrong")
-				}
-				return data
-			} catch (error) {
-				throw new Error(error.meassage)
-			}
-		}
-	})
+  const { data: suggestedUsers, isLoading } = useQuery({
+    queryKey: ["suggestedUsers"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/user/suggested");
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.message || "Something went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error.meassage);
+      }
+    },
+  });
 
-	if(suggestedUsers?.length === 0) return <div className="md:w-64 w-0"></div>
+  const { follow, isPending } = useFollow();
+
+  if (suggestedUsers?.length === 0) return <div className="md:w-64 w-0"></div>;
 
   return (
     <div className="hidden lg:block my-4 mx-2">
@@ -60,9 +66,12 @@ export default function RightPanel() {
                 <div>
                   <button
                     className="btn bg-white text-black hover:bg-white hover:opacity-90 rounded-full btn-sm"
-                    onClick={(e) => e.preventDefault()}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      follow(user._id);
+                    }}
                   >
-                    Follow
+                    {isPending ? <LoadingSpinner size="sm" /> : "Follow"}
                   </button>
                 </div>
               </Link>
